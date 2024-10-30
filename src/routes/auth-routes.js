b13f7e675321ea971178083434f88f7e9de39806
Pisek-K <pisekkidsawanon@gmail.com>
2024-10-30 16:45:19 +0700

    debugging_authorized

--- src/routes/auth-routes.js
+++ src/routes/auth-routes.js
@@ -1,14 +1,14 @@
 const { express } = require("../model")
 const router = express.Router();
-const {register,login} = require('../controllers/index');
+const {register,login} = require('../controllers');
 
 
 
 
 
 
-router.post('/register', ()=>{})
-router.post('/login', ()=>{})
+router.post('/register', register)
+router.post('/login', login)
 
 
 module.exports =  router
