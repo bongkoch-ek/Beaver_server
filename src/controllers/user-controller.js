75e65f9e72128280572b8922aa99f3d24e1f765a
Bongkoch Ekphachaisawat <ekp.bongkoch@gmail.com>
2024-11-12 13:06:04 +0700

    add column in new project

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -3,28 +3,28 @@ const createError = require("../utils/createError");
 
 // Get a user by ID
 exports.getUser = async (req, res, next) => {
-    try {
-      const { id } = req.params;
-  
-      if (!id || isNaN(id)) {
-        return next(createError(400, "Invalid user ID"));
-      }
-  
-      const user = await prisma.user.findUnique({
-        where: { 
-          id: parseInt(id) 
-        },
-      });
-  
-      if (!user) {
-        return next(createError(404, "User not found"));
-      }
-  
-      res.status(200).json(user);
-    } catch (err) {
-      next(err);
+  try {
+    const { id } = req.params;
+
+    if (!id || isNaN(id)) {
+      return next(createError(400, "Invalid user ID"));
     }
-  };
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id: parseInt(id)
+      },
+    });
+
+    if (!user) {
+      return next(createError(404, "User not found"));
+    }
+
+    res.status(200).json(user);
+  } catch (err) {
+    next(err);
+  }
+};
 
 
 
@@ -48,9 +48,9 @@ exports.listUser = async (req, res, next) => {
 // Update user profile
 exports.updateProfile = async (req, res, next) => {
   try {
-    const { displayname, firstname, lastname, profileImage, bio, phone} =
+    const { displayname, firstname, lastname, profileImage, bio, phone } =
       req.body;
-      console.log("check body update :",req.body);
+    console.log("check body update :", req.body);
     const userId = req.user.id;
     const user = await prisma.user.update({
       where: { id: userId },
@@ -62,10 +62,10 @@ exports.updateProfile = async (req, res, next) => {
         profileImage: profileImage,
       },
     });
-    console.log("check user update :",user)
+    console.log("check user update :", user)
     res
       .status(200)
-      .json({ message: `Updated user ${user.displayName} successfully`, user : user });
+      .json({ message: `Updated user ${user.displayName} successfully`, user: user });
   } catch (err) {
     next(err);
   }
@@ -86,58 +86,65 @@ exports.deleteUser = async (req, res, next) => {
 
 // Create a project
 exports.createProject = async (req, res, next) => {
-    try {
-      const { projectName, images , asset_id , public_id , url , secure_url } = req.body; 
-      const userId = req.user.id; 
-      console.log("Images received:", images); 
-  
-      if (!userId) {
-        return next(createError(403, "User not authenticated"));
-      }
-  
-      console.log("check project name :", projectName); 
-  
-      const project = await prisma.project.create({
-        data: {
-          projectName: projectName,
-          userId: userId,
-          images: {
-            create: {
-              asset_id: asset_id,
-              public_id: public_id,
-              url: url,
-              secure_url: secure_url,
-            },
-          }
-        },
-      });
-  
-      await prisma.groupProject.create({
-        data: {
-          userId: userId,
-          projectId: project.id,
-          role: "OWNER",
-          status : "ACTIVE"
-        }
-      });
-  
-      const userResponse = {
-        id: userId,
-        email: req.user.email, 
-      };
-  
-      res.status(201).json({
-        message: `Created project ${project.projectName} successfully`,
-        project: {
-          id: project.id,
-          projectName: project.projectName,
-          user: userResponse, 
-          images: project.images,
-        },
-      });
-    } catch (err) {
-      console.error("Error creating project:", err); 
-      next(err);
+  try {
+    const { projectName, images, asset_id, public_id, url, secure_url } = req.body;
+    const userId = req.user.id;
+    console.log("Images received:", images);
+
+    if (!userId) {
+      return next(createError(403, "User not authenticated"));
     }
-  };
-  
+
+    console.log("check project name :", projectName);
+
+    const project = await prisma.project.create({
+      data: {
+        projectName: projectName,
+        userId: userId,
+        images: {
+          create: {
+            asset_id: asset_id,
+            public_id: public_id,
+            url: url,
+            secure_url: secure_url,
+          },
+        }
+      },
+    });
+
+    await prisma.groupProject.create({
+      data: {
+        userId: userId,
+        projectId: project.id,
+        role: "OWNER",
+        status: "ACTIVE"
+      }
+    });
+
+    const list = await prisma.list.createMany({
+      data: [
+        { title: "To do", projectId: project.id, userId: userId, status: "TODO" },
+        { title: "In Progress", projectId: project.id, userId: userId, status: "INPROGRESS" },
+        { title: "Done", projectId: project.id, userId: userId, status: "DONE" },
+      ]
+    });
+
+    const userResponse = {
+      id: userId,
+      email: req.user.email,
+    };
+
+    res.status(201).json({
+      message: `Created project ${project.projectName} successfully`,
+      project: {
+        id: project.id,
+        projectName: project.projectName,
+        user: userResponse,
+        images: project.images,
+      },
+    });
+  } catch (err) {
+    console.error("Error creating project:", err);
+    next(err);
+  }
+};
